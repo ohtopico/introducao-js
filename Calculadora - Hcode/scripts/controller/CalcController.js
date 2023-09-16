232df6a57f51ce7685bc4fc3437a2958aeffe968
Júlia R. Quoos Alves <juliarqalves@gmail.com>
2023-09-15 23:29:13 -0300

    update

--- Calculadora - Hcode/scripts/controller/CalcController.js
+++ Calculadora - Hcode/scripts/controller/CalcController.js
@@ -1,33 +1,35 @@
 class CalcController {
 
     constructor(){
+        this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
-        this._dateEl = document.querySelector("#data");
-        this._timeEl = document.querySelector("#hora");
+        this._dateEl = document.querySelector("#date");
+        this._timeEl = document.querySelector("#hour");
         this._currentDate;
         this.initialize();
     }
 
     initialize(){
-        this._displayCalcEl.innerHTML = "0";
-        this._dateEl.innerHTML = "1/1/2023";
-        this._timeEl.innerHTML =  "00:00";
+        setInterval(()=> {
+            this.displayDate = this.currentDate.toDateString(this._locale);
+            this.displayTime = this.currentDate.toTimeString(this._locale);
+        }, 1000); //atualiza a data a cada 1 segundo
     }
 
     get displayDate(){
         return this._dateEl.innerHTML;
     }
 
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
+    }
+
     get displayTime(){
         return this._timeEl.innerHTML;
     }
 
-    get displayCalc(){
-        return this._displayCalcEl.innerHTML;
-    }
-
-    set displayCalc(value){
-        this._displayCalcEl.innerHTML = value;
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
     }
 
     get currentDate(){
@@ -36,6 +38,14 @@ class CalcController {
 
     set currentDate(value){
         this._currentDate = value;
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
     }
 
 }
